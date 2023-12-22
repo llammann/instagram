3e40362cbca83404e1fbd95e710dcfc26e3cf671
llammann <bd7n2v9tr@code.edu.az>
2023-12-22 19:50:58 +0400

    ig

---
+++ admin/src/App.tsx
@@ -0,0 +1,17 @@
+import Tablex from "./components/Table";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>go to hell</h1>
+      <section className="table">
+        <div className="container">
+          <Tablex />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
